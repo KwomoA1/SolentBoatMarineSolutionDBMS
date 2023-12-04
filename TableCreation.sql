25fa53277f20153f6151eb98f67c6fc7db4c53a7
Zachary <97988282+wherearemysocks11@users.noreply.github.com>
2023-12-04 16:28:02 +0000

    WE ARE STUPID

--- TableCreation.sql
+++ TableCreation.sql
@@ -58,67 +58,67 @@ CREATE TABLE dock_details(
 );
 
 CREATE TABLE 
-    staff_details(
-        staff_id SERIAL PRIMARY KEY,
-        boatyard_id INT NOT NULL REFERENCES boatyard_details (boatyard_id),
-        staff_fname VARCHAR(40) NOT NULL,
-        staff_lname  VARCHAR(40) NOT NULL,
-        dob DATE NOT NULL,
-        address1 VARCHAR (60) NOT NULL,
-        address2 VARCHAR (50),
-        town VARCHAR(60) NOT NULL,
-        postcode CHAR (15) NOT NULL,
-        mobile_number  CHAR(15) NOT NULL,
-        landline_number CHAR(15),
-        personal_emailaddress VARCHAR(100),
-        work_emailaddress VARCHAR(100) NOT NULL
-);
+  staff_details(
+    staff_id SERIAL PRIMARY KEY,
+    boatyard_id INT NOT NULL REFERENCES boatyard_details (boatyard_id),
+    staff_fname VARCHAR(40) NOT NULL,
+    staff_lname  VARCHAR(40) NOT NULL,
+    dob DATE NOT NULL,
+    address1 VARCHAR (60) NOT NULL,
+    address2 VARCHAR (50),
+    town VARCHAR(60) NOT NULL,
+    postcode CHAR (15) NOT NULL,
+    mobile_number  CHAR(15) NOT NULL,
+    landline_number CHAR(15),
+    personal_emailaddress VARCHAR(100),
+    work_emailaddress VARCHAR(100) NOT NULL
+  );
 
 CREATE TABLE 
-    roles(
-        role_id SERIAL PRIMARY KEY,
-        role_name VARCHAR (30) NOT NULL
-    );
+  roles(
+    role_id SERIAL PRIMARY KEY,
+    role_name VARCHAR (30) NOT NULL
+  );
 
 CREATE TABLE
-    staff_roles(
-        staff_id INT REFERENCES staff_details (staff_id),
-        role_id SMALLINT REFERENCES roles (role_id)
-    );
+  staff_roles(
+    staff_id INT REFERENCES staff_details (staff_id),
+    role_id SMALLINT REFERENCES roles (role_id)
+  );
 
 CREATE TYPE storageType AS ENUM ('Wet Slip', 'Dry Slip', 'Indoors');
 CREATE TYPE boatSize AS ENUM ('Small', 'Medium', 'Large');
 CREATE TYPE boatType AS ENUM ('Commercial', 'Private');
 
 CREATE TABLE
-    boats(
-        boat_id SERIAL PRIMARY KEY,
-        customer_id INTEGER NOT NULL REFERENCES customer_details (customer_id),
-        boat_yard_id INTEGER NOT NULL REFERENCES boatyard_details ( boatyard_id),
-        dock_id INTEGER NOT NULL REFERENCES dock_details (dock_id),
-        boat_storage_type storageType NOT NULL,
-        boat_size_class boatSize NOT NULL,
-        boat_type boatType NOT NULL,
-        boat_name VARCHAR (80) NOT NULL,
-        model VARCHAR (40) NOT NULL,
-        build_date DATE,
-        length_overall VARCHAR (20) NOT NULL,
-        beam VARCHAR (20) NOT NULL,
-        draft VARCHAR (20) NOT NULL,
-        cargo/tankers_capacity VARCHAR (15) NOT NULL
+  boats(
+    boat_id SERIAL PRIMARY KEY,
+    customer_id INTEGER NOT NULL REFERENCES customer_details (customer_id),
+    boat_yard_id INTEGER NOT NULL REFERENCES boatyard_details ( boatyard_id),
+    dock_id INTEGER NOT NULL REFERENCES dock_details (dock_id),
+    boat_storage_type storageType NOT NULL,
+    boat_size_class boatSize NOT NULL,
+    boat_type boatType NOT NULL,
+    boat_name VARCHAR (80) NOT NULL,
+    model VARCHAR (40) NOT NULL,
+    build_date DATE,
+    length_overall VARCHAR (20) NOT NULL,
+    beam VARCHAR (20) NOT NULL,
+    draft VARCHAR (20) NOT NULL,
+    cargo/tankers_capacity VARCHAR (15) NOT NULL
     );
 
 CREATE TABLE
-    classes(
-        class_id SERIAL PRIMARY KEY,
-        class_name VARCHAR (40) NOT NULL
-    );
+  classes(
+    class_id SERIAL PRIMARY KEY,
+    class_name VARCHAR (40) NOT NULL
+  );
 
 CREATE TABLE
-    boat_classes(
-        boat_id INT NOT NULL REFERENCES boat_details (boat_id),
-        class_id INT NOT NULL REFERENCES classes (class_id)
-    );
+  boat_classes(
+    boat_id INT NOT NULL REFERENCES boat_details (boat_id),
+    class_id INT NOT NULL REFERENCES classes (class_id)
+  );
 
 CREATE TABLE hull_materials( 
     hull_material_id SERIAL PRIMARY KEY, 
