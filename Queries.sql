47c055b119dfb18d4d25ae95e8baf7987872880d
TopLadRobin <147411227+TopLadRobin@users.noreply.github.com>
2023-12-11 21:20:13 +0000

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,5 +1,6 @@
-SELECT customer_details.customer_id, customer_details.business_name
+SELECT customer_details.customer_id, customer_details.business_name, boats.boat_id, boats.boat_name
 FROM customer_details
-LEFT JOIN boats
+JOIN boats
 ON customer_details.customer_id = boats.customer_id
-WHERE customer_details.customer_type = ("Business")
+WHERE customer_details.customer_type = ('Business')
+ORDER BY customer_details.customer_id;
