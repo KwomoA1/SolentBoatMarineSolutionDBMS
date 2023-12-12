7917f9b1d497812189bfcc625bf70c695eff1195
TopLadRobin <147411227+TopLadRobin@users.noreply.github.com>
2023-12-12 01:18:51 +0000

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -1,6 +1,23 @@
-SELECT customer_details.customer_id, customer_details.business_name, boats.boat_id, boats.boat_name
+--Query 4
+
+-- For when you need to look up the capacity of the docks in a dockyard in a specific country. (This scenario is Poland)
+SELECT boatyard_details.boatyard_name AS "Boatyard Name", dock_details.dock_id AS "Dock ID", dock_details.wet_slips_max_capcity AS "Wet Slip Capacity",dock_details.dry_slips_max_capacity AS "Dry Slip Capacity"
+FROM boatyard_details
+JOIN dock_details
+ON boatyard_details.boatyard_id = dock_details.boatyard_id
+WHERE boatyard_details.country = ('Poland')
+ORDER BY dock_details.dock_id;
+
+
+--Query 5
+
+-- For when you need to see all of the boats that are owned by businesses rather than individuals not affiliated with a business
+SELECT customer_details.customer_id AS "Customer ID", customer_details.business_name AS "Business Name", boats.boat_id AS "Boat ID", boats.boat_name AS "Boat Name"
 FROM customer_details
 JOIN boats
 ON customer_details.customer_id = boats.customer_id
 WHERE customer_details.customer_type = ('Business')
 ORDER BY customer_details.customer_id;
+
+
+
