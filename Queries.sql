7d8d3be8468b0afc1b86f9fda5bcc4605d33a10d
TopLadRobin <147411227+TopLadRobin@users.noreply.github.com>
2023-12-12 01:48:26 +0000

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -20,4 +20,14 @@ WHERE customer_details.customer_type = ('Business')
 ORDER BY customer_details.customer_id;
 
 
+--Query 6 
 
+-- This query allows you to search up a customer by their ID and then find the boats under their name/assosiated with them as well as it's booking status.
+SELECT customer_details.customer_id AS "Customer ID", boats.boat_id AS "Boat ID", boats.boat_name AS "Boat Name", boats.boat_size_class AS "Boat Class", boats.model AS "Boat Model", bookings.booking_status AS "Booking Status"
+FROM customer_details
+JOIN boats
+ON customer_details.customer_id = boats.customer_id
+JOIN bookings
+ON customer_details.customer_id = bookings.customer_id
+WHERE customer_details.customer_id = 14
+ORDER BY boats.boat_id;
