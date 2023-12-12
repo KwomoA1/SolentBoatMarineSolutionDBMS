42aba5b360324c2d0d59769f0a8cd513774ec899
Zac <zacwstill@gmail.com>
2023-12-12 01:11:48 +0000

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -14,8 +14,10 @@ WHERE boatyard_details.boatyard_name = 'Blue Bill Park';
 /* Query 2 is designed so that managers can list all scheduled tasks in the future,
 this can help with scheduling and procurement */
 
-SELECT booking_id, boat_id, boatyard_id, booking_date, issue_description
+SELECT booking_id AS "Booking ID", boat_name AS "Boat", boatyard_name AS "Boatyard", booking_date AS "Booking Date", issue_description AS "Issue Description"
 FROM bookings
+JOIN boats ON boats.boat_id = bookings.boat_id
+JOIN boatyard_details ON boatyard_details.boatyard_id = bookings.boatyard_id
 WHERE booking_type = 'Pre-booked' AND booking_status = 'Scheduled';
 
 /* Query 3 */
@@ -24,9 +26,10 @@ WHERE booking_type = 'Pre-booked' AND booking_status = 'Scheduled';
 in this case, boat mechanics, are available. This has then been split into how many are on each site,
 as this can then be used to help decide which place is best for certain jobs for ships */
 
-SELECT count(staff_roles.staff_id) AS "Number of Boat Mechanics", boatyard_id AS "Site"
+SELECT count(staff_roles.staff_id) AS "Number of Boat Mechanics", boatyard_details.boatyard_name AS "Site"
 FROM staff_roles
-JOIN ROLES ON roles.role_id = staff_roles.role_id
+JOIN roles ON roles.role_id = staff_roles.role_id
 JOIN staff_details ON staff_roles.staff_id = staff_details.staff_id
+JOIN boatyard_details ON boatyard_details.boatyard_id = staff_details.boatyard_id
 WHERE roles.role_name = 'Boat Mechanic'
-GROUP BY boatyard_id;
+GROUP BY boatyard_details.boatyard_id;
