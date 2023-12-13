a73883960fb287e59483e322f0f0d90bbd29a4dc
TopLadRobin <147411227+TopLadRobin@users.noreply.github.com>
2023-12-13 22:17:18 +0000

    Update roles.sql

--- roles.sql
+++ roles.sql
@@ -16,10 +16,10 @@ CREATE ROLE manager WITH LOGIN PASSWORD 'dbmanager1';
 ---------------------
 -- Manager Permission
 ---------------------
-GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE staff_details, staff_roles, boatyard_id, bookings_service, bookings, customer_details, emergency_contact, boats, boat_classes, classes, boat_hullmaterials, hull_materials, boat_engines, engines, fuel_type TO manager;
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE staff_details, staff_roles, boatyard_details, booking_service, booking, customer_details, emergency_contact, boat_details, boat_classes, classes, boat_hullmaterials, hull_materials, boats_engine, engines, fuel_type TO manager;
 
-GRANT SELECT, INSERT, UPDATE, DELETE ON SCHEMA public TO manager WITH GRANT OPTION;
-REVOKE SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public FROM manager;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO manager WITH GRANT OPTION;
+--REVOKE SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public FROM manager;
 
 GRANT SELECT ON staff_boatyard_view, boatyard_storage_availabllity_view, Business_Storing_Boats_view, indivudals_Storing_Boats_view, completed_bookings_view, ongoing_bookings_view, scheduled_bookings_view, commercial_ships_stored_view, private_ships_stored_view TO manager;
 -------------------------------
@@ -35,9 +35,9 @@ CREATE ROLE maintaince_crew;
 -------------------------------
 -- Maintaince crew permission 
 --------------------------------
-GRANT SELECT ON TABLE bookings, customer_details, emergency_contact, boats, boat_classes, boat_hullmaterials, boat_engines, engines, fuel_type, classes, hull_materials TO maintaince_crew;
-GRANT INSERT ON TABLE bookings, bookings_service TO maintaince_crew;
-GRANT UPDATE ON TABLE bookings_service, bookings, boat_hullmaterials, boat_engines, TO maintaince_crew;
+GRANT SELECT ON TABLE booking, customer_details, emergency_contact, boat_details, boat_classes, boat_hullmaterials, boats_engine, engines, fuel_types, classes, hull_materials TO maintaince_crew;
+GRANT INSERT ON TABLE booking, booking_service TO maintaince_crew;
+GRANT UPDATE ON TABLE booking_service, booking, boat_hullmaterials, boats_engine TO maintaince_crew;
 
 GRANT SELECT ON boatyard_storage_availabllity_view, Business_Storing_Boats_view, indivudals_Storing_Boats_view, completed_bookings_view, ongoing_bookings_view, scheduled_bookings_view, commercial_ships_stored_view, private_ships_stored_view TO maintaince_crew;
 ---------------------------
@@ -49,7 +49,7 @@ CREATE ROLE customer_assistant WITH LOGIN PASSWORD 'dbcustomerassistant1';
 -- customer assistant permission 
 --------------------------------
 
-GRANT SELECT ON TABLE bookings, customer_details, emergency_contact, boats, boat_classes, boat_hullmaterials, boat_engines, classes, hull_materials, engines, fuel_type TO customer_assistant;
-GRANT INSERT, UPDATE ON TABLE bookings, customer_details, emergency_contact, boats, boat_classes, boat_hullmaterials, boat_engines TO customer_assistant;
+GRANT SELECT ON TABLE booking, customer_details, emergency_contact, boat_details, boat_classes, boat_hullmaterials, boats_engine, classes, hull_materials, engines, fuel_types TO customer_assistant;
+GRANT INSERT, UPDATE ON TABLE booking, customer_details, emergency_contact, boat_details, boat_classes, boat_hullmaterials, boats_engine TO customer_assistant;
 
 GRANT SELECT ON boatyard_storage_availabllity_view, Business_Storing_Boats_view, indivudals_Storing_Boats_view, completed_bookings_view, ongoing_bookings_view, scheduled_bookings_view, commercial_ships_stored_view, private_ships_stored_view TO customer_assistant;
